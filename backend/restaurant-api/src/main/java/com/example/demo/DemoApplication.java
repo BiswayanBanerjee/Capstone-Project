fe16c18ea1c70f89e4d344dc1a85b3065127c34d
Biswayan Banerjee <89703859+BiswayanBanerjee@users.noreply.github.com>
2024-12-08 14:33:44 +0530

    Update DemoApplication.java

--- backend/restaurant-api/src/main/java/com/example/demo/DemoApplication.java
+++ backend/restaurant-api/src/main/java/com/example/demo/DemoApplication.java
@@ -15,39 +15,6 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
-    // @Bean
-    // public FilterRegistrationBean<CorsFilter> filterRegistrationBean() {
-    //     final CorsConfiguration config = new CorsConfiguration();
-
-    //     // Allow credentials to be sent in CORS requests
-    //     config.setAllowCredentials(true);
-
-    //     // Get allowed origins from environment variable or set default value
-    //     String allowedOrigins = System.getenv("ALLOWED_ORIGINS");
-    //     if (allowedOrigins != null) {
-    //         // Split the comma-separated list of allowed origins and add them
-    //         for (String origin : allowedOrigins.split(",")) {
-    //             config.addAllowedOrigin(origin.trim());
-    //         }
-    //     } else {
-    //         // Default to allowing localhost in development
-    //         config.addAllowedOrigin("http://localhost:3000");
-    //     }
-
-    //     // Allow all headers and methods
-    //     config.addAllowedHeader("*");
-    //     config.addAllowedMethod("*");
-
-    //     // Set the CORS configuration to apply to all URLs
-    //     final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    //     source.registerCorsConfiguration("/**", config);
-
-    //     // Register the CORS filter with high precedence
-    //     FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-    //     bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-    //     return bean;
-    // }
         @Bean
 	public FilterRegistrationBean<CorsFilter> filterRegistrationBean() {
     		final CorsConfiguration config = new CorsConfiguration();
@@ -62,6 +29,6 @@ public class DemoApplication {
     		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
     		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
     		return bean;
-}
+	}
 }
 
